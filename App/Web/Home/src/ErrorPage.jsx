646d8a2b192e22f65d0d9ed8ddd3392d1eaa86b6
Mouaad <93816869+sMouaad@users.noreply.github.com>
2024-03-19 21:43:29 +0100

    Change border in error page

--- App/Web/Home/src/ErrorPage.jsx
+++ App/Web/Home/src/ErrorPage.jsx
@@ -2,7 +2,7 @@ import Lottie from "lottie-react";
 import animation from "./assets/error.json";
 const ErrorPage = () => {
   return (
-    <div className="py-10 border-4 border-primary shadow-inner shadow-green-400 px-4 h-screen flex flex-col justify-center items-center">
+    <div className="py-10 border-primary shadow-inner shadow-green-400 px-4 h-screen flex flex-col justify-center items-center">
       <h1 className="font-Poppins text-center text-3xl">
         Oh no, this page does not exist!
       </h1>
