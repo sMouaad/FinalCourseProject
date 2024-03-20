18fefc9f809bc1f548160272c965e34a23ff8623
Mouaad <93816869+sMouaad@users.noreply.github.com>
2024-03-20 23:18:54 +0100

    Add initial boilerplate for chat section

---
+++ App/Web/Home/src/components/ChatInput.jsx
@@ -0,0 +1,31 @@
+import { MdSend } from "react-icons/md";
+import { useState } from "react";
+
+export default function ChatInput({ disabled, onSend }) {
+  const [message, setMessage] = useState("");
+  return (
+    <form
+      className="border-2 border-green-400 rounded-lg p-2 flex flex-row m-2"
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSend(message);
+        setMessage("");
+      }}
+      autoComplete="off"
+    >
+      <input
+        name="message"
+        placeholder="Start talking with me!"
+        className="w-full focus:outline-none "
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button
+        disabled={disabled}
+        className="bg-Chat transition-all font-bold py-2 px-4 rounded hover:bg-green-400 disabled:bg-green-800 disabled:text-slate-400"
+      >
+        <MdSend />
+      </button>
+    </form>
+  );
+}
---
+++ App/Web/Home/src/components/ChatStatusIndicator.jsx
@@ -0,0 +1,14 @@
+import { SiOpenai } from "react-icons/si";
+
+export default function ChatStatusIndicator({ status }) {
+  return (
+    <div className="flex justify-center">
+      <div className="flex flex-row items-center text-green-300">
+        <div className="m-2 animate-spin">
+          <SiOpenai />
+        </div>
+        <div>{status}</div>
+      </div>
+    </div>
+  );
+}
