7a64f92c51506eca7a0c82d40e3b6eab0e05db83
Mouaad <93816869+sMouaad@users.noreply.github.com>
2024-03-21 12:50:48 +0100

    Add responsiveness even more

--- App/Web/Home/src/Sidebar.jsx
+++ App/Web/Home/src/Sidebar.jsx
@@ -9,40 +9,41 @@ import { Link } from "react-router-dom";
 import { GoCopilot } from "react-icons/go";
 function Sidebar() {
   return (
-    <div className="fixed z-10 h-screen bg-BgColor w-20 sidebar-shadow">
-      <div className="relative z-10 gap-12 rounded-xl top-10 left-1 sm:left-7 h-sidebar w-16 m-0 flex flex-col bg-primary text-white shadow-lg pt-4 pb-4">
-        <SidebarIcon clickable={false} icon={<img src={Brain} />} />
-        <div className="flex flex-col gap-1">
-          <Link to="/">
-            <SidebarIcon msg={"Home"} icon={<FaHome size="30" />} />
-          </Link>
-          <Link to="/apps">
-            <SidebarIcon msg={"Apps"} icon={<IoIosApps size="30" />} />
-          </Link>
-          <Link to="/games">
-            <SidebarIcon msg={"Games"} icon={<IoGameController size="30" />} />
-          </Link>
-          <Link to="/notifications">
-            <SidebarIcon
-              msg={"Notifications"}
-              icon={<RiNotification3Fill size="30" />}
-            />
-          </Link>
-        </div>
-        <div className="flex flex-col">
-          <Link to="/assistant">
-            <SidebarIcon
-              msg={"Virtual Assistant"}
-              icon={<GoCopilot size="30" />}
-            />
-          </Link>
-          <Link to="/to-do-list">
-            <SidebarIcon msg={"To-do list"} icon={<LuListTodo size="30" />} />
-          </Link>
-        </div>
-        <div className="mt-auto">
-          <SidebarIcon msg={"Exit"} icon={<LuLogOut size="30" />} />
-        </div>
+    <div className="fixed z-10 sm:gap-10 sm:rounded-xl sm:top-10 sm:left-7 sm:h-sidebar w-screen sm:w-16 m-0 flex sm:flex-col bg-primary text-white shadow-lg sm:pt-4 sm:pb-4">
+      <SidebarIcon clickable={false} icon={<img src={Brain} />} />
+      <div className="flex sm:flex-col sm:gap-1">
+        <Link to="/">
+          <SidebarIcon
+            msg={"Home"}
+            icon={<FaHome className="size-0 sm:size-8" />}
+          />
+        </Link>
+        <Link to="/apps">
+          <SidebarIcon msg={"Apps"} icon={<IoIosApps size="30" />} />
+        </Link>
+        <Link to="/games">
+          <SidebarIcon msg={"Games"} icon={<IoGameController size="30" />} />
+        </Link>
+        <Link to="/notifications">
+          <SidebarIcon
+            msg={"Notifications"}
+            icon={<RiNotification3Fill size="30" />}
+          />
+        </Link>
+      </div>
+      <div className="flex sm:flex-col">
+        <Link to="/assistant">
+          <SidebarIcon
+            msg={"Virtual Assistant"}
+            icon={<GoCopilot size="30" />}
+          />
+        </Link>
+        <Link to="/to-do-list">
+          <SidebarIcon msg={"To-do list"} icon={<LuListTodo size="30" />} />
+        </Link>
+      </div>
+      <div className="sm:mt-auto">
+        <SidebarIcon msg={"Exit"} icon={<LuLogOut size="30" />} />
       </div>
     </div>
   );
