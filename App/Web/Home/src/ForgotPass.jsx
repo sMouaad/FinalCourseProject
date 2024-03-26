278045caa80a089d396aa67063e479ddc91c57ce
Mouaad <93816869+sMouaad@users.noreply.github.com>
2024-03-26 14:18:47 +0100

    Add mongodb & RESTful api with authentification

---
+++ App/Web/Home/src/ForgotPass.jsx
@@ -0,0 +1,7 @@
+export default function ForgotPassword() {
+  return (
+    <div className="flex h-screen bg-[#f0f2f5] items-center justify-center">
+      <div className="w-[768px] max-w-full min-h-[480px] rounded-[30px] border-4 border-black"></div>
+    </div>
+  );
+}
--- App/Web/Home/src/Login.jsx
+++ App/Web/Home/src/Login.jsx
@@ -1,9 +1,46 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import brain from "./assets/cerveau_idea.png";
 import { FaFacebook } from "react-icons/fa";
 import { AiFillGoogleCircle } from "react-icons/ai";
+import Axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Login() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailLogin, setEmailLogin] = useState("");
+  const [passwordLogin, setPasswordLogin] = useState("");
+  const navigate = useNavigate();
+
+  Axios.defaults.withCredentials = true;
+  const handleSubmitRegister = (e) => {
+    e.preventDefault();
+    Axios.post("http://localhost:3000/auth/signup", { name, email, password })
+      .then((res) => {
+        if (res.data.status) navigate("/home");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const handleSubmitLogin = (e) => {
+    e.preventDefault();
+    console.log("what");
+    Axios.post("http://localhost:3000/auth/login", {
+      emailLogin,
+      passwordLogin,
+    })
+      .then((res) => {
+        console.log(res);
+        if (res.data.status) navigate("/home");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   useEffect(() => {
     const container = document.getElementById("container");
     const registerBtn = document.getElementById("register");
@@ -25,7 +62,10 @@ export default function Login() {
         id="container"
       >
         <div className="form-container left-0 w-1/2 opacity-0 z-[1] sign-up">
-          <form className="bg-white flex items-center justify-center flex-col py-0 px-[40px] h-full">
+          <form
+            onSubmit={handleSubmitRegister}
+            className="bg-white flex items-center justify-center flex-col py-0 px-[40px] h-full"
+          >
             <h1 className="text-3xl font-bold">Create Account</h1>
             <div className="my-[20px] mx-0">
               <a
@@ -49,25 +89,44 @@ export default function Login() {
             <input
               className="bg-[#eee] border-none my-[8px] mx-0 py-[10px] px-[15px] text-[13px] rounded-[8px] w-full outline-none"
               type="text"
+              name="name"
               placeholder="Name"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
             />
             <input
               className="bg-[#eee] border-none my-[8px] mx-0 py-[10px] px-[15px] text-[13px] rounded-[8px] w-full outline-none"
               type="email"
+              name="email"
               placeholder="e-mail"
+              autoComplete="off"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
             />
             <input
               className="bg-[#eee] border-none my-[8px] mx-0 py-[10px] px-[15px] text-[13px] rounded-[8px] w-full outline-none"
               type="password"
+              name="password"
               placeholder="Password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
             />
-            <button className=" bg-[#00e5bd] text-white text-[12px] py-[10px] px-[45px] border-[1px] border-transparent rounded-[8px] font-[600] tracking-[0.5px] uppercase mt-[10px] cursor-pointer">
+            <button
+              type="submit"
+              className=" bg-[#00e5bd] text-white text-[12px] py-[10px] px-[45px] border-[1px] border-transparent rounded-[8px] font-[600] tracking-[0.5px] uppercase mt-[10px] cursor-pointer"
+            >
               Sign Up
             </button>
           </form>
         </div>
         <div className="absolute top-0 h-full transition-all duration-[0.6s] ease-in-out left-0 w-1/2 z-[2] sign-in">
-          <form className="bg-white flex items-center justify-center flex-col py-0 px-[40px] h-full">
+          <form
+            onSubmit={handleSubmitLogin}
+            className="bg-white flex items-center justify-center flex-col py-0 px-[40px] h-full"
+          >
             <h1 className="text-3xl font-bold">Sign In</h1>
             <div className="my-[20px] mx-0">
               <a
@@ -87,22 +146,35 @@ export default function Login() {
             </div>
             <span className=" text-[12px]">or use your email password</span>
             <input
+              name="emailLogin"
               className="bg-[#eee] border-none my-[8px] mx-0 py-[10px] px-[15px] text-[13px] rounded-[8px] w-full outline-none"
               type="email"
               placeholder="Email"
+              autoComplete="off"
+              onChange={(e) => {
+                setEmailLogin(e.target.value);
+              }}
             />
             <input
+              name="passwordLogin"
               className="bg-[#eee] border-none my-[8px] mx-0 py-[10px] px-[15px] text-[13px] rounded-[8px] w-full outline-none"
               type="password"
               placeholder="Password"
+              onChange={(e) => {
+                setPasswordLogin(e.target.value);
+              }}
             />
-            <a
+            <Link
+              to="forgot-password"
               className="text-[#333] text-[13px] no-underline mt-[15px] mx-0 mb-[10px]"
               href="#"
             >
-              Forget Your Password?
-            </a>
-            <button className=" bg-[#00e5bd] text-white text-[12px] py-[10px] px-[45px] border-[1px] border-transparent rounded-[8px] font-[600] tracking-[0.5px] uppercase mt-[10px] cursor-pointer">
+              Forgot your password?
+            </Link>
+            <button
+              type="submit"
+              className=" bg-[#00e5bd] text-white text-[12px] py-[10px] px-[45px] border-[1px] border-transparent rounded-[8px] font-[600] tracking-[0.5px] uppercase mt-[10px] cursor-pointer"
+            >
               Sign In
             </button>
           </form>
@@ -129,7 +201,8 @@ export default function Login() {
               />
               <h1 className="text-3xl font-bold">Dhakira</h1>
               <p className="text-[14px] tracking-[0.3px] leading-[20px] my-5 mx-0">
-                Register with your personal details to use all of site features
+                Don&apos;t have an account? Click on the register button and
+                join us now!
               </p>
               <button
                 className="toggle-right bg-[#58cc02] text-white text-[12px] py-[10px] px-[45px] border-[1px] border-transparent rounded-[8px] font-[600] tracking-[0.5px] uppercase mt-[10px] cursor-pointer"
