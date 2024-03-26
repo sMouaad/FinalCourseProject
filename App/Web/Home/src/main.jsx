6ec883b9e1ed42e8a12c6988ac6fe9bff58c3a06
Mouaad <93816869+sMouaad@users.noreply.github.com>
2024-03-26 09:58:13 +0100

    Fix routing

--- App/Web/Home/src/main.jsx
+++ App/Web/Home/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Login from "./Login";
+import Router from "./Router";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Login />
+    <Router />
   </React.StrictMode>
 );
