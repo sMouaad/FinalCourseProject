bda29b8b2a066be63d9ae0372e6550e95923c4d7
Mouaad <93816869+sMouaad@users.noreply.github.com>
2024-03-27 10:53:39 +0100

    Add expired reset link page

--- App/Web/Home/src/ResetPassword.jsx
+++ App/Web/Home/src/ResetPassword.jsx
@@ -6,6 +6,7 @@ import Axios from "axios";
 
 export default function ResetPassword() {
   const [password, setPassword] = useState("");
+  const [expired, setExpired] = useState(false);
   const navigate = useNavigate();
   const { token } = useParams();
   const handleSubmit = (e) => {
@@ -22,44 +23,49 @@ export default function ResetPassword() {
       })
       .catch((err) => {
         console.log(err);
+        setExpired(true);
       });
   };
   return (
     <div className="flex h-screen bg-[#f0f2f5] items-center justify-center">
-      <div className="relative flex flex-col w-[768px] bg-white shadow-lg max-w-full min-h-[480px] rounded-[30px]">
-        <Link
-          className="flex absolute items-center left-8 text-slate-700 hover:text-green-400 transition-all ease-linear top-8 gap-2 cursor-pointer h-fit"
-          to="/login"
-        >
-          <FaArrowLeft /> Go back
-        </Link>
-        <div className="flex gap-12 flex-col my-auto h-full justify-center items-center">
-          <div>
-            <h1 className="text-3xl font-bold">Reset Your Password</h1>
-          </div>
-          <form id="forget" action="" onSubmit={handleSubmit}>
-            <label htmlFor="password">New Password:</label>
-            <input
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-              id="password"
-              type="password"
-              className="bg-[#eee] border-none my-[8px] mx-0 py-[10px] px-[15px] text-[13px] rounded-[8px] w-full outline-none"
-              placeholder="•••••••••••••"
-            />
-            <div className="flex justify-center">
-              <button
-                form="forget"
-                type="submit"
-                className=" bg-[#00e5bd] text-white text-[12px] py-[10px] px-[45px] border-[1px] border-transparent rounded-[8px] font-[600] tracking-[0.5px] uppercase mt-[10px] cursor-pointer"
-              >
-                Send
-              </button>
+      {!expired ? (
+        <div className="relative flex flex-col w-[768px] bg-white shadow-lg max-w-full min-h-[480px] rounded-[30px]">
+          <Link
+            className="flex absolute items-center left-8 text-slate-700 hover:text-green-400 transition-all ease-linear top-8 gap-2 cursor-pointer h-fit"
+            to="/login"
+          >
+            <FaArrowLeft /> Go back
+          </Link>
+          <div className="flex gap-12 flex-col my-auto h-full justify-center items-center">
+            <div>
+              <h1 className="text-3xl font-bold">Reset Your Password</h1>
             </div>
-          </form>
+            <form id="forget" action="" onSubmit={handleSubmit}>
+              <label htmlFor="password">New Password:</label>
+              <input
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                id="password"
+                type="password"
+                className="bg-[#eee] border-none my-[8px] mx-0 py-[10px] px-[15px] text-[13px] rounded-[8px] w-full outline-none"
+                placeholder="•••••••••••••"
+              />
+              <div className="flex justify-center">
+                <button
+                  form="forget"
+                  type="submit"
+                  className=" bg-[#00e5bd] text-white text-[12px] py-[10px] px-[45px] border-[1px] border-transparent rounded-[8px] font-[600] tracking-[0.5px] uppercase mt-[10px] cursor-pointer"
+                >
+                  Send
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
+      ) : (
+        <p>Reset link has expired...</p>
+      )}
     </div>
   );
 }
