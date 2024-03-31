38131caf9a60d0a7848901051a0cf7fbcead06b0
Mahdjoubabderraouf <mahdjoub.abderraouf@gmail.com>
2024-03-31 08:02:58 +0100

    Animate "Welcom to Dhakira " using reanimted

--- App/Mobile/pasient-app/Comp/key-page/Key.jsx
+++ App/Mobile/pasient-app/Comp/key-page/Key.jsx
@@ -1,15 +1,60 @@
+import React, { useState, useEffect } from "react";
 import { View, Text, TextInput } from "react-native";
 import Button from "../Button";
 import { Image } from "expo-image";
+import Animated, {
+  useSharedValue,
+  withDelay,
+  withTiming,
+} from "react-native-reanimated";
 import welcomToDhakira from "../../assets/images/welcomToDhakira.png";
-
+const DURATION = 1000;
+const DELAY = 500;
+const text = ["Welcome ", "to ", "Dhakira..."];
 const Key = () => {
+  const [isShown, setShown] = useState(false);
+
+  const opacity1 = useSharedValue(0);
+  const opacity2 = useSharedValue(0);
+  const opacity3 = useSharedValue(0);
+
+  // prettier-ignore
+  const show = () => {
+    if (isShown) {
+    opacity3.value = withDelay(0 * DELAY, withTiming(0, { duration: DURATION }));
+    opacity2.value = withDelay(1 * DELAY, withTiming(0, { duration: DURATION }));
+    opacity1.value = withDelay(2 * DELAY, withTiming(0, { duration: DURATION }));
+  } else {
+    opacity1.value = withDelay(0 * DELAY, withTiming(1, { duration: DURATION }));
+    opacity2.value = withDelay(1 * DELAY, withTiming(1, { duration: DURATION }));
+    opacity3.value = withDelay(2 * DELAY, withTiming(1, { duration: DURATION }));
+  }
+  };
+  show();
   return (
     <View className="h-screen py-[20] gap-20">
       <View className="items-center justify-start flex-col ">
-        <Text className="font-bold text-[32px] text-[#00A588] mb-[10px]">
-          Welcome to Dhakira...
-        </Text>
+        <View className="flex flex-row">
+          <Animated.Text
+            className="font-bold text-[32px] text-[#00A588] mb-[10px]"
+            style={{ opacity: opacity1 }}
+          >
+            {text[0]}
+          </Animated.Text>
+          <Animated.Text
+            className="font-bold text-[32px] text-[#00A588] mb-[10px]"
+            style={{ opacity: opacity2 }}
+          >
+            {text[1]}
+          </Animated.Text>
+
+          <Animated.Text
+            className="font-bold text-[32px] text-[#00A588] mb-[10px]"
+            style={{ opacity: opacity3 }}
+          >
+            {text[2]}
+          </Animated.Text>
+        </View>
         <Image
           source={welcomToDhakira}
           className="rounded-2xl w-[171] h-[173] "
