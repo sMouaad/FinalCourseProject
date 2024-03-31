dea33cd2d552a2645bfaf1ed54e1341aba721a4e
Mahdjoubabderraouf <mahdjoub.abderraouf@gmail.com>
2024-03-31 09:35:31 +0100

    key page finiched

--- App/Mobile/pasient-app/Comp/key-page/Key.jsx
+++ App/Mobile/pasient-app/Comp/key-page/Key.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TextInput } from "react-native";
-import Button from "../Button";
+import { View, Text, TextInput, Pressable } from "react-native";
 import { Image } from "expo-image";
 import Animated, {
   useSharedValue,
@@ -10,9 +9,11 @@ import Animated, {
 import welcomToDhakira from "../../assets/images/welcomToDhakira.png";
 const DURATION = 1000;
 const DELAY = 500;
+let bol = false;
 const text = ["Welcome ", "to ", "Dhakira..."];
 const Key = () => {
   const [isShown, setShown] = useState(false);
+  const [inputValue, setInputValue] = useState("");
 
   const opacity1 = useSharedValue(0);
   const opacity2 = useSharedValue(0);
@@ -24,32 +25,36 @@ const Key = () => {
     opacity3.value = withDelay(0 * DELAY, withTiming(0, { duration: DURATION }));
     opacity2.value = withDelay(1 * DELAY, withTiming(0, { duration: DURATION }));
     opacity1.value = withDelay(2 * DELAY, withTiming(0, { duration: DURATION }));
+
   } else {
     opacity1.value = withDelay(0 * DELAY, withTiming(1, { duration: DURATION }));
     opacity2.value = withDelay(1 * DELAY, withTiming(1, { duration: DURATION }));
     opacity3.value = withDelay(2 * DELAY, withTiming(1, { duration: DURATION }));
+
   }
   };
+
   show();
+
   return (
     <View className="h-screen py-[20] gap-20">
       <View className="items-center justify-start flex-col ">
-        <View className="flex flex-row">
+        <View className="flex flex-row mb-2">
           <Animated.Text
-            className="font-bold text-[32px] text-[#00A588] mb-[10px]"
+            className="font-bold text-[30px] text-[#00A588] mb-[10px]"
             style={{ opacity: opacity1 }}
           >
             {text[0]}
           </Animated.Text>
           <Animated.Text
-            className="font-bold text-[32px] text-[#00A588] mb-[10px]"
+            className="font-bold text-[30px] text-[#00A588] mb-[10px]"
             style={{ opacity: opacity2 }}
           >
             {text[1]}
           </Animated.Text>
 
           <Animated.Text
-            className="font-bold text-[32px] text-[#00A588] mb-[10px]"
+            className="font-bold text-[30px] text-[#00A588] mb-[10px]"
             style={{ opacity: opacity3 }}
           >
             {text[2]}
@@ -65,12 +70,46 @@ const Key = () => {
         <TextInput
           placeholder="Enter the keyword"
           className="border-2 items-center border-Primary px-[20] py-[15] m-[10] rounded-[20px]"
+          value={inputValue}
+          onChangeText={setInputValue}
         ></TextInput>
-        <Button btnClassName="items-center bg-Primary p-[12] m-[10] rounded-[20px]">
-          <View className="bg-[#00A588] w-full items-center py-1 rounded-[20px]">
-            <Text className="text-white font-bold text-3xl ">Start</Text>
+        <Pressable
+          onPress={() => {
+            setInputValue("");
+          }}
+          btnClassName="items-center bg-Primary p-[12] m-[10] rounded-[20px]"
+          style={({ pressed }) => [
+            {
+              alignItems: "center",
+              backgroundColor: "#00E5BD",
+              padding: 12,
+              margin: 10,
+              borderRadius: 20,
+              shadowColor: "black",
+              shadowOpacity: 0.3,
+              shadowOffset: { width: 0, height: 2 },
+              shadowRadius: 4,
+              elevation: 5,
+            },
+            pressed && { elevation: 1, opacity: 0.9 },
+          ]}
+        >
+          <View
+            style={[
+              {
+                backgroundColor: "#00A588",
+                width: "100%",
+                alignItems: "center",
+                paddingVertical: 1,
+                borderRadius: 20,
+              },
+            ]}
+          >
+            <Text style={{ color: "white", fontWeight: "bold", fontSize: 20 }}>
+              Start
+            </Text>
           </View>
-        </Button>
+        </Pressable>
       </View>
     </View>
   );
