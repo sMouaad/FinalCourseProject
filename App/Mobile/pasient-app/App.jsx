d8fdbf309356b13a7c556fee60040b52cdbf2f91
Mahdjoubabderraouf <mahdjoub.abderraouf@gmail.com>
2024-04-01 22:10:36 +0100

    update in the solution of issue #6 make the WaitingForTheFaceRecognition page not availibale when the user come back to the page after the face recognition is done.

--- App/Mobile/pasient-app/App.jsx
+++ App/Mobile/pasient-app/App.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  TouchableWithoutFeedback,
-  KeyboardAvoidingView,
-  Keyboard,
-  View,
-} from "react-native";
+
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -14,30 +9,25 @@ import WaitingForTheFaceRecognition from "./Comp/WaitingForTheFaceRecognition/Wa
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
+const App = () => {
   return (
     <NavigationContainer>
-      <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
-        <View className="flex-1 flex justify-center box-border h-screen px-[10px]">
-          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <Stack.Navigator
-              screenOptions={{
-                headerShown: false,
-                contentStyle: { backgroundColor: "transparent" },
-              }}
-            >
-              <Stack.Screen name="Home" component={Key} />
-              <Stack.Screen name="Test" component={Test} />
-              <Stack.Screen
-                name="WaitingPage"
-                component={WaitingForTheFaceRecognition}
-                options={{ unmountOnBlur: true }}
-              />
-            </Stack.Navigator>
-          </TouchableWithoutFeedback>
-        </View>
-      </KeyboardAvoidingView>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: "transparent" },
+        }}
+      >
+        <Stack.Screen name="Home" component={Key} />
+        <Stack.Screen name="Test" component={Test} />
+        <Stack.Screen
+          name="WaitingPage"
+          component={WaitingForTheFaceRecognition}
+        />
+      </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
   );
-}
+};
+export default App;
+2;
