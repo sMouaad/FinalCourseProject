f5636123e50b008e444882f0878143b7ebdb1b0b
Mahdjoubabderraouf <mahdjoub.abderraouf@gmail.com>
2024-04-04 09:37:02 +0100

    make the navigation using scroling or by pressing in the specific TabButton

---
+++ App/Mobile/pasient-app/NavigationAfterLogin/Screens/Chat/BackgroundColorContext.js
@@ -0,0 +1,3 @@
+import { createContext } from "react";
+
+export const BackgroundColorContext = createContext("#00E5BD");
--- App/Mobile/pasient-app/NavigationAfterLogin/Screens/Chat/TabBar.jsx
+++ App/Mobile/pasient-app/NavigationAfterLogin/Screens/Chat/TabBar.jsx
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import TabButton from "./TabButton";
 import React, { useState } from "react";
 
-const TabBar = ({ activeTab, setActiveTab, bgColor }) => {
+const TabBar = ({ activeTab, setActiveTab, onPress1, onPress2, onPress3 }) => {
   // const [activeTab, setActiveTab] = useState("all");
 
   const handleTabClick = (tab) => {
@@ -12,20 +12,28 @@ const TabBar = ({ activeTab, setActiveTab, bgColor }) => {
     <View className=" m-[20] bg-[#F2F2F2] justify-evenly content-center flex-row rounded-3xl shadow-md shadow-black">
       <TabButton
         active={activeTab === "all"}
-        onPress={() => handleTabClick("all")}
-        bgColor={bgColor}
+        onPress={() => {
+          handleTabClick("all");
+          onPress1();
+        }}
       >
         All Chats
       </TabButton>
       <TabButton
         active={activeTab === "group"}
-        onPress={() => handleTabClick("group")}
+        onPress={() => {
+          handleTabClick("group");
+          onPress2();
+        }}
       >
         Group
       </TabButton>
       <TabButton
         active={activeTab === "assistants"}
-        onPress={() => handleTabClick("assistants")}
+        onPress={() => {
+          handleTabClick("assistants");
+          onPress3();
+        }}
       >
         Assistants
       </TabButton>
