4d331eab2200cb397c8fa5e95d6ad3263ecf2215
YounesBensafia <younes.bensafia@etu.usthb.dz>
2024-04-05 17:44:43 +0100

    Fix the First Page

--- App/Mobile/Dhakira_assistant_app/NavigationAfterLogin/Screens/HomeScreens/Settings.js
+++ App/Mobile/Dhakira_assistant_app/NavigationAfterLogin/Screens/HomeScreens/Settings.js
@@ -3,10 +3,31 @@ import React from 'react'
 
 export default function Settings() {
   return (
-    <View>
-      <Text>Settings</Text>
+    
+    <View style={{flex:1, justifyContent:'center', alignItems: 'center'}}>
+        <View style={styles.Delete}></View>
+        <View style={{flexDirection:'row'}}>
+            <View style={styles.buttons}></View>
+            <View style={styles.buttons}></View>
+        </View>
+        <View style={{flexDirection:'row'}}>
+            <View style={styles.buttons}></View>
+            <View style={styles.buttons}></View>
+        </View>
+        <View style={styles.Delete}></View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+buttons:
+{
+    backgroundColor:'#fff', width:150,height:150, marginHorizontal: 10, marginVertical:10, borderRadius:31,justifyContent:'center', alignItems: 'center', borderWidth:5, borderColor:'#5E60CE'
+},
+Delete:
+{
+    backgroundColor:'#fff', width:310,height:60, marginHorizontal: 10, marginVertical:10, borderRadius:31,justifyContent:'center', alignItems: 'center', borderWidth:5, borderColor:'red'
+}
+
+})
