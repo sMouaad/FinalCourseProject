56455ad7ceb4bdb978027bb35dc865743b79be14
YounesBensafia <younes.bensafia@etu.usthb.dz>
2024-04-05 14:48:27 +0100

    Adding the Settings Screen and the Track Screen

---
+++ App/Mobile/Dhakira_assistant_app/NavigationAfterLogin/Screens/HomeScreens/Track.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Track() {
+  return (
+    <View>
+      <Text>Track</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
