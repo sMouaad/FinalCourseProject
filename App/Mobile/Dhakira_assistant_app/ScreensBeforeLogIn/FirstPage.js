300ce06fbd1f8301d8892e2325ea2aee2757930d
YounesBensafia <younes.bensafia@etu.usthb.dz>
2024-04-09 17:32:09 +0100

    test

--- App/Mobile/Dhakira_assistant_app/ScreensBeforeLogIn/FirstPage.js
+++ App/Mobile/Dhakira_assistant_app/ScreensBeforeLogIn/FirstPage.js
@@ -9,7 +9,7 @@ function First_Page({navigation})
       <View  style={styles.container}>
         <Image style={styles.dhakiraImage} source={require('../Images/DhakiraCool.png')} />
         <Text  style={styles.textImage}>Dhakira</Text>
-        <Text  style={styles.textSousImage}>La methode gratuite, fun, et efficace pour t'assister dans tes taches</Text>
+        <Text  style={styles.textSousImage}>La méthode gratuite, amusante et efficace pour t'assister dans tes tâches</Text>
         <ButtonCestParti onPress={() => navigation.navigate('SignUpPage')} text={"C'EST PARTI"}/>
         <ButtonJaiDejaUnCompte onPress = {() => navigation.navigate('LoginPage')} text ={"J'AI DEJA UN COMPTE"}/>
       </View>
