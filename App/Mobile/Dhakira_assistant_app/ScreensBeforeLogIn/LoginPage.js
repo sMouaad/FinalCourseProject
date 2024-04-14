c578eed63f454b01a14991e51b21ae6825665a05
Mouaad <93816869+sMouaad@users.noreply.github.com>
2024-04-14 13:41:49 +0100

    Link sign up front-end of mobile to backend

--- App/Mobile/Dhakira_assistant_app/ScreensBeforeLogIn/LoginPage.js
+++ App/Mobile/Dhakira_assistant_app/ScreensBeforeLogIn/LoginPage.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import {Text, StyleSheet, Pressable, View, TextInput, TouchableOpacity} from 'react-native';
-import {useNavigation } from '@react-navigation/native';
+import React from "react";
+import {
+  Text,
+  StyleSheet,
+  Pressable,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import Axios from "axios";
 
-function LoginPageInterface ()
-{
-  const  navigation = useNavigation();
+function LoginPageInterface() {
+  const navigation = useNavigation();
   return (
     <>
       <View style={styles.container}>
@@ -12,93 +19,104 @@ function LoginPageInterface ()
           <View style={styles.Secondcontainer}></View>
         </View>
 
-        <TextInput placeholder="Enter your Username" style={styles.username}/>
-        <TextInput placeholder="Enter your Paswword"  style={styles.inputPassword}  secureTextEntry={true} />
-        <TouchableOpacity style={styles.loginButton} onPress={()=> navigation.navigate('Main')}><Text style={styles.TextLoginButton}>Log in</Text></TouchableOpacity>
-        <Text style={{ color: '#6930C3' , fontWeight: 'bold', marginTop:10, fontSize:20}}>You don't have account?, <Text style={{color:'#42b72a', textDecorationLine: 'underline'}} onPress={() => navigation.navigate('SignUpPage')}>Sign Up.</Text></Text>
+        <TextInput placeholder="Enter your Username" style={styles.username} />
+        <TextInput
+          placeholder="Enter your Paswword"
+          style={styles.inputPassword}
+          secureTextEntry={true}
+        />
+        <TouchableOpacity
+          style={styles.loginButton}
+          onPress={() => navigation.navigate("Main")}
+        >
+          <Text style={styles.TextLoginButton}>Log in</Text>
+        </TouchableOpacity>
+        <Text
+          style={{
+            color: "#6930C3",
+            fontWeight: "bold",
+            marginTop: 10,
+            fontSize: 20,
+          }}
+        >
+          You don't have account?,{" "}
+          <Text
+            style={{ color: "#42b72a", textDecorationLine: "underline" }}
+            onPress={() => navigation.navigate("SignUpPage")}
+          >
+            Sign Up.
+          </Text>
+        </Text>
       </View>
-    
     </>
-
   );
 }
 const styles = StyleSheet.create({
-  Firstcontainer:
-  {
+  Firstcontainer: {
     marginTop: -200,
     marginBottom: 200,
-    width: '90%',
+    width: "90%",
     height: 20,
-    backgroundColor: '#6930C3',
+    backgroundColor: "#6930C3",
     borderRadius: 22,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
-  Secondcontainer:
-  {
-    width: '90%',
+  Secondcontainer: {
+    width: "90%",
     height: 8,
-    backgroundColor: '#72569D',
+    backgroundColor: "#72569D",
     borderRadius: 22,
   },
-  container:
-  {
+  container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
   },
-  inputPassword: 
-  {
-    borderColor: '#6930C3',
+  inputPassword: {
+    borderColor: "#6930C3",
     width: "90%",
     height: 60,
     borderWidth: 4,
     borderBottomLeftRadius: 22,
-    borderBottomRightRadius: 22, 
-    textAlign: 'center',
+    borderBottomRightRadius: 22,
+    textAlign: "center",
     marginBottom: 20,
-    fontSize:20
+    fontSize: 20,
   },
-  username:
-  {
-    borderColor: '#6930C3',
+  username: {
+    borderColor: "#6930C3",
     width: "90%",
     height: 60,
     borderLeftWidth: 4,
     borderRightWidth: 4,
     borderTopWidth: 4,
     borderTopLeftRadius: 22,
-    borderTopRightRadius: 22, 
-    textAlign: 'center',
-    fontSize:20
+    borderTopRightRadius: 22,
+    textAlign: "center",
+    fontSize: 20,
   },
-  loginButton:
-  {
+  loginButton: {
     width: "90%",
     height: 60,
-    borderRadius: 22, 
-    backgroundColor: '#6930C3',
-    justifyContent: 'center'
-    
+    borderRadius: 22,
+    backgroundColor: "#6930C3",
+    justifyContent: "center",
   },
-  TextLoginButton:
-  {
-    textAlign: 'center',
+  TextLoginButton: {
+    textAlign: "center",
     fontSize: 20,
-    fontWeight: 'bold',
-    color: '#fff'
+    fontWeight: "bold",
+    color: "#fff",
   },
-  FirstText:
-  {
+  FirstText: {
     marginTop: -10,
     marginBottom: 20,
     fontSize: 20,
-    fontWeight:'bold',
-    color: "#4F278D"
-  }
-}
-);
+    fontWeight: "bold",
+    color: "#4F278D",
+  },
+});
 
-
-export {LoginPageInterface};
+export { LoginPageInterface };
--- App/Mobile/Dhakira_assistant_app/ScreensBeforeLogIn/SignUpPage.js
+++ App/Mobile/Dhakira_assistant_app/ScreensBeforeLogIn/SignUpPage.js
@@ -1,118 +1,194 @@
-import React from 'react';
-import {View, Text, StyleSheet, Image, Pressable, Button, TextInput, TouchableOpacity} from 'react-native';
-import {useNavigation } from '@react-navigation/native';
-function SignUpPageInter()
-{
-  const  navigation = useNavigation();
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Pressable,
+  Button,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+import Axios from "axios";
+function SignUpPageInter() {
+  const navigation = useNavigation();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirm] = useState("");
   return (
     <View>
-
-      <View  style={styles.Firstcontainer}>
-        <View  style={styles.ChargingBar}><View style={{width:'100%', height:22, backgroundColor:'#6930C3', borderRadius:8, justifyContent: 'center'}}><View style={{width:'100%', height:12, backgroundColor:'#72569D', borderRadius:8}}></View></View></View>
-        <TextInput  style={styles.FirstInputSignUp} placeholder='Enter your First Name' inputMode='text'></TextInput>
-        <TextInput  style={styles.InputSignUpPrenom} placeholder='Enter your Last Name' inputMode='text'></TextInput>
-        <TextInput  style={styles.InputSignUpS} placeholder=' Enter your Gmail' inputMode='email'></TextInput>
-        <TextInput  style={styles.InputSignUpS} placeholder=' Enter your Phone Number' inputMode='tel'></TextInput>
-        <TextInput  style={styles.InputSignUpS} placeholder='Password' secureTextEntry={true}></TextInput>
-        <TextInput  style={styles.LastInputSignUp} placeholder='Confirm you password' secureTextEntry={true}></TextInput>
-        <Text style={styles.TextLogin}>Already have account?, <Text style={{color:'#42b72a', textDecorationLine: 'underline'}} onPress={() => navigation.navigate('LoginPage')}>Login</Text></Text>
-        <TouchableOpacity style={styles.NextButton} title><Text style={styles.TextInNButton}>Next</Text></TouchableOpacity>
-        
-        
+      <View style={styles.Firstcontainer}>
+        <View style={styles.ChargingBar}>
+          <View
+            style={{
+              width: "100%",
+              height: 22,
+              backgroundColor: "#6930C3",
+              borderRadius: 8,
+              justifyContent: "center",
+            }}
+          >
+            <View
+              style={{
+                width: "100%",
+                height: 12,
+                backgroundColor: "#72569D",
+                borderRadius: 8,
+              }}
+            ></View>
+          </View>
+        </View>
+        <TextInput
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          style={styles.FirstInputSignUp}
+          placeholder="Enter your Name"
+          inputMode="text"
+        ></TextInput>
+        <TextInput
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          style={styles.InputSignUpS}
+          placeholder=" Enter your E-mail"
+          inputMode="email"
+        ></TextInput>
+        <TextInput
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          style={styles.InputSignUpS}
+          placeholder="Password"
+          secureTextEntry={true}
+        ></TextInput>
+        <TextInput
+          onChange={(e) => {
+            setConfirm(e.target.value);
+          }}
+          style={styles.LastInputSignUp}
+          placeholder="Confirm your password"
+          secureTextEntry={true}
+        ></TextInput>
+        <Text style={styles.TextLogin}>
+          Already have account?,{" "}
+          <Text
+            style={{ color: "#42b72a", textDecorationLine: "underline" }}
+            onPress={() => navigation.navigate("LoginPage")}
+          >
+            Login
+          </Text>
+        </Text>
+        <TouchableOpacity
+          style={styles.NextButton}
+          title
+          onPress={() => {
+            if (confirmPassword === password) {
+              Axios.post("http://localhost:3000/auth/signup", {
+                name,
+                email,
+                password,
+              })
+                .then((res) => {
+                  if (res.data.status) {
+                    const accessToken = res?.data?.accessToken;
+                    setAuth({ accessToken });
+                    navigation.navigate("Main");
+                  }
+                })
+                .catch((err) => {
+                  alert(err);
+                });
+            } else {
+              alert("password not same");
+            }
+          }}
+        >
+          <Text style={styles.TextInNButton}>Next</Text>
+        </TouchableOpacity>
       </View>
     </View>
-
-
   );
 }
 const styles = StyleSheet.create({
-  Firstcontainer:
-  {
+  Firstcontainer: {
     flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    marginTop:35,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    marginTop: 35,
   },
-  NextButton:
-  {
-    width:"90%",
-    height:60,
-    backgroundColor: '#6930C3',
+  NextButton: {
+    width: "90%",
+    height: 60,
+    backgroundColor: "#6930C3",
     borderRadius: 22,
-    justifyContent: 'center'
+    justifyContent: "center",
   },
-  TextInNButton:
-  {
-    textAlign: 'center',
+  TextInNButton: {
+    textAlign: "center",
     fontSize: 20,
-    fontWeight: 'bold',
-    color: '#fff'
+    fontWeight: "bold",
+    color: "#fff",
   },
-  ChargingBar:
-  {
-    width:'90%', 
-    height:22, 
-    backgroundColor:'#D9D9D9', 
-    borderRadius:8,
-    marginBottom: '50%',
+  ChargingBar: {
+    width: "90%",
+    height: 22,
+    backgroundColor: "#D9D9D9",
+    borderRadius: 8,
+    marginBottom: "50%",
   },
-  InputSignUpPrenom:
-  {
-    width:"90%",
-    height:60,
+  InputSignUpPrenom: {
+    width: "90%",
+    height: 60,
     borderLeftWidth: 4,
     borderRightWidth: 4,
     fontSize: 20,
-    borderColor:'#6930C3',
-    textAlign: 'center'
-    
+    borderColor: "#6930C3",
+    textAlign: "center",
   },
-  InputSignUpS:
-  {
-    width:"90%",
-    height:60,
+  InputSignUpS: {
+    width: "90%",
+    height: 60,
     borderLeftWidth: 4,
     borderRightWidth: 4,
     borderTopWidth: 4,
-    borderColor:'#6930C3',
+    borderColor: "#6930C3",
     fontSize: 20,
-    textAlign: 'center'
-    
+    textAlign: "center",
   },
-  FirstInputSignUp:
-  {
-    width:"90%",
-    height:60,
+  FirstInputSignUp: {
+    width: "90%",
+    height: 60,
     borderWidth: 4,
     borderTopLeftRadius: 22,
-    borderColor:'#6930C3',
+    borderColor: "#6930C3",
     borderTopRightRadius: 22,
     fontSize: 20,
-    textAlign: 'center',
-    marginTop:-120
+    textAlign: "center",
+    marginTop: -120,
   },
-  LastInputSignUp:
-  {
-    width:"90%",
-    height:60,
+  LastInputSignUp: {
+    width: "90%",
+    height: 60,
     borderWidth: 4,
-    borderColor:'#6930C3',
+    borderColor: "#6930C3",
     borderBottomLeftRadius: 22,
     borderBottomRightRadius: 22,
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     marginBottom: 20,
   },
-  TextLogin:
-  {
-    width:"90%",
-    height:60,
-    color:'#6930C3',
+  TextLogin: {
+    width: "90%",
+    height: 60,
+    color: "#6930C3",
     fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  }
-}
-);
-export {SignUpPageInter}
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
+export { SignUpPageInter };
