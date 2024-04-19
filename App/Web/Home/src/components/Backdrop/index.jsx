4e16fb351e0cb8aeeaae85233348275dfc658c51
Mouaad <93816869+sMouaad@users.noreply.github.com>
2024-04-19 17:54:24 +0100

    Fix modal layout by changing it to fixed

--- App/Web/Home/src/components/Backdrop/index.jsx
+++ App/Web/Home/src/components/Backdrop/index.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 const Backdrop = ({ children, onClick }) => {
   return (
     <motion.div
-      className="absolute z-[100] h-full w-full top-0 left-0 bg-[#000000e1] flex items-center justify-center"
+      className="fixed z-[100] h-full w-full top-0 left-0 bg-[#000000e1] flex items-center justify-center"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
