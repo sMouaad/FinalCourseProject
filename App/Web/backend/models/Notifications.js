74cb577db9f704b8b89e6a927fd819d9fcc5839f
Mouaad <93816869+sMouaad@users.noreply.github.com>
2024-04-30 10:39:11 +0100

    Add invitation logic backend & add receiver field in Notifications

--- App/Web/backend/models/Notifications.js
+++ App/Web/backend/models/Notifications.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 const NotificationSchema = new mongoose.Schema({
   message: { type: String },
   sender: { type: mongoose.SchemaTypes.ObjectId, required: true },
-  patients: [{ type: mongoose.SchemaTypes.ObjectId }],
+  receiver: { type: mongoose.SchemaTypes.ObjectId, required: true },
+  patient: { type: mongoose.SchemaTypes.ObjectId, required: true },
 });
 const NotificationModel = mongoose.model("Notification", NotificationSchema);
 export { NotificationModel as Notification };
