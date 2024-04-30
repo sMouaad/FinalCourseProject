d13a7a61c570d599540551217bf692162ea0e5d4
Mouaad <93816869+sMouaad@users.noreply.github.com>
2024-04-30 10:11:13 +0100

    Add new collection Notifications

---
+++ App/Web/backend/models/Notifications.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+const NotificationSchema = new mongoose.Schema({
+  message: { type: String },
+  sender: { type: mongoose.SchemaTypes.ObjectId, required: true },
+});
+const NotificationModel = mongoose.model("Notification", NotificationSchema);
+export { NotificationModel as Notification };
