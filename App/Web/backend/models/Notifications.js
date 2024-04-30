5060a581c52da7d2be2ec0ebe55c98aa93fe88f2
Mouaad <93816869+sMouaad@users.noreply.github.com>
2024-04-30 10:22:40 +0100

    Add new field patients to notifications

--- App/Web/backend/models/Notifications.js
+++ App/Web/backend/models/Notifications.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 const NotificationSchema = new mongoose.Schema({
   message: { type: String },
   sender: { type: mongoose.SchemaTypes.ObjectId, required: true },
+  patients: [{ type: mongoose.SchemaTypes.ObjectId }],
 });
 const NotificationModel = mongoose.model("Notification", NotificationSchema);
 export { NotificationModel as Notification };
