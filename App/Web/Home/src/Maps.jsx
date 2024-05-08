5357e265cecf0c04bf6124c62e002b626c375fac
Mouaad <93816869+sMouaad@users.noreply.github.com>
2024-05-08 16:08:49 +0100

    Change position

--- App/Web/Home/src/Maps.jsx
+++ App/Web/Home/src/Maps.jsx
@@ -1,3 +1,18 @@
+import { useState } from "react";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+} from "@vis.gl/react-google-maps";
 export default function Maps() {
-  return <div>Maps!</div>;
+  const position = { lat: 36.68351349669975, lng: 3.0717719900568627 };
+  return (
+    <APIProvider apiKey="AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg">
+      <div className="h-screen">
+        <Map zoom={9} center={position}></Map>
+      </div>
+    </APIProvider>
+  );
 }
