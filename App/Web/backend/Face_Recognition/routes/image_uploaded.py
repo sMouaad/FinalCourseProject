f20fdec4340f2b3e33ebcc18c484a0b72778ad4b
Mahdjoubabderraouf <mahdjoub.abderraouf@gmail.com>
2024-05-16 14:56:32 +0100

    add get api endpoint lets the caregiver to get images of patient using his id

--- App/Web/backend/Face_Recognition/routes/image_uploaded.py
+++ App/Web/backend/Face_Recognition/routes/image_uploaded.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter, Body, File, Form, UploadFile, HTTPException
 from fastapi import APIRouter
 import os
 
+from fastapi.responses import FileResponse
 from pydantic import Field
-from config.database import patient_collection
+from config.database import patient_collection, users_collection
 from models.patient import Patient
 
 from schema.schemas import patient_serial, patients_serial, read_patient_image
@@ -12,6 +13,7 @@ from numpy import array
 
 
 from detect import search_encoding_in_list, one_face_to_encoding
+
 
 router = APIRouter()
 
@@ -28,7 +30,7 @@ async def process_image_patient(
     #     raise HTTPException(status_code=401, detail="Invalid token")
     # Validate file type (optional)
 
-    upload_dir = "uploads"
+    upload_dir = "uploadsPatient"
 
     if file.content_type not in ("image/jpeg", "image/png"):
         raise HTTPException(status_code=400, detail="Unsupported image type")
@@ -48,11 +50,13 @@ async def process_image_patient(
 
         if len(list_encodings) != 0:
             i = search_encoding_in_list(full_path, list_encodings)
+            os.remove(full_path)
             return {
                 "name ": list_encodings[i]["name"],
                 "This is your ": list_encodings[i]["who"],
             }
         else:
+            os.remove(full_path)
             return {"No data found"}
 
     except Exception as e:
@@ -81,7 +85,7 @@ async def process_image_caregiver(
     try:
 
         fetched_patient = await patient_collection.find_one({"_id": ObjectId(id)})
-        patinet = patient_serial(fetched_patient)
+        # patinet = patient_serial(fetched_patient)
         content = await file.read()
         filename = file.filename
 
@@ -95,27 +99,54 @@ async def process_image_caregiver(
         if isinstance(encoding, str):
             return {"message": encoding}
 
-        if isinstance(search_encoding_in_list(full_path, patinet["images"]), str):
+        if isinstance(
+            search_encoding_in_list(full_path, fetched_patient["images"]), str
+        ):
 
-            patinet["images"].append({"name": Name, "who": Who, "encoding": encoding})
+            fetched_patient["images"].append(
+                {"name": Name, "who": Who, "encoding": encoding, "url": filename}
+            )
 
             await patient_collection.update_one(
-                {"_id": ObjectId(id)}, {"$set": patinet}
+                {"_id": ObjectId(id)}, {"$set": fetched_patient}
             )
-            return {"message": Name + " is added!", "image": full_path}
-        return {"message": Name + " already exist ! ", "image": full_path}
+            return {"message": Name + " is added!"}
+        return {"message": Name + " already exist ! "}
 
     except Exception as e:
         raise print(e)
         # raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
 
 
-@router.put("/caregiver_update_image")
+@router.put("/update_image")
 async def update_image():
     return
 
 
-@router.get("/get_all_patients")
-async def get_all_patients():
-    patients = await patient_collection.find().to_list(length=1000)
-    return patients_serial(patients)
+@router.get("/person_image/{url}")
+async def patients_images(
+    url: str,
+):
+    file_path = os.path.join("uploads", url)
+    return FileResponse(
+        file_path, media_type="image/jpeg", filename=os.path.basename(url)
+    )
+
+
+@router.get("/patient_images")
+async def patients_images(
+    id: str,
+):
+    fetched_patient = await patient_collection.find_one({"_id": ObjectId(id)})
+    images = fetched_patient["images"]
+    images_url = list(
+        map(
+            lambda image: {
+                "name": image["name"],
+                "who": image["who"],
+                "url": image["url"],
+            },
+            images,
+        )
+    )
+    return images_url
