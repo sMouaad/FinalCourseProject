ff2f95872aa1d93f0773561eaccfe8e0f71704ee
Mouaad <93816869+sMouaad@users.noreply.github.com>
2024-05-18 18:01:57 +0100

    Add marker to maps

--- App/Web/Home/src/Maps.jsx
+++ App/Web/Home/src/Maps.jsx
@@ -2,16 +2,20 @@ import { useState } from "react";
 import {
   APIProvider,
   Map,
+  Marker,
   AdvancedMarker,
   Pin,
   InfoWindow,
 } from "@vis.gl/react-google-maps";
+import Brain from "./assets/pointer.svg";
 export default function Maps() {
   const position = { lat: 36.68351349669975, lng: 3.0717719900568627 };
   return (
     <APIProvider apiKey="AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg">
       <div className="h-screen">
-        <Map zoom={9} center={position}></Map>
+        <Map zoom={9} center={position}>
+          <Marker position={position} icon={Brain}></Marker>
+        </Map>
       </div>
     </APIProvider>
   );
