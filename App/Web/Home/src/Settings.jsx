5b6f09c0cc6100d4939e228c86334751ae18b53d
Mouaad <93816869+sMouaad@users.noreply.github.com>
2024-05-24 11:30:21 +0100

    Add a hidden file input for image upload and link it to motion button via label

--- App/Web/Home/src/Settings.jsx
+++ App/Web/Home/src/Settings.jsx
@@ -33,94 +33,108 @@ export default function Settings() {
             <p className="text-slate-600">&#40;{email}&#41;</p>
           </div>
           <img className="bg-yellow-300 rounded-full" src={ProfilePic} alt="" />
+
           <motion.button
             whileTap={{ scale: 0.95 }}
-            className=" bg-[#008de5] hover:bg-[#0184cb] text-white text-[12px] border-[1px] px-4 border-transparent min-h-8 rounded-full font-[600] tracking-[0.5px] uppercase cursor-pointer transition-all ease-linear duration-100"
+            className=" bg-[#008de5] hover:bg-[#0184cb] py-2 text-white text-[12px] border-[1px] border-transparent rounded-full font-[600] tracking-[0.5px] uppercase cursor-pointer transition-all ease-linear duration-100"
           >
-            Change Profile Picture
+            <input id="profilepic" className="size-0" type="file" name="" />
+            <label
+              className="h-full w-full py-2 px-16 hover:cursor-pointer"
+              htmlFor="profilepic"
+            >
+              Change Profile Picture
+            </label>
           </motion.button>
         </div>
         <div className="my-auto">
-          <div className="flex pb-12 items-center justify-center gap-32">
-            <div className="flex flex-col">
-              <label className="text-sm font-bold text-slate-600" htmlFor="">
-                Full Name
-              </label>
-              <input
-                className="my-[8px] rounded-[8px] outline-none"
-                type="text"
-                placeholder="Name"
-                autoComplete="false"
-              />
+          <form autoComplete="false">
+            <div className="flex pb-12 items-center justify-center gap-32">
+              <div className="flex flex-col">
+                <label className="text-sm font-bold text-slate-600" htmlFor="">
+                  Full Name
+                </label>
+                <input
+                  className="my-[8px] rounded-[8px] outline-none"
+                  type="text"
+                  placeholder="Name"
+                  value={name}
+                  autoComplete="false"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="text-sm font-bold text-slate-600" htmlFor="">
+                  Phone Number
+                </label>
+                <input
+                  className="my-[8px] rounded-[8px] outline-none"
+                  type="tel"
+                  placeholder="+213xxxxxxxx"
+                  autoComplete="false"
+                />
+              </div>
             </div>
-            <div className="flex flex-col">
-              <label className="text-sm font-bold text-slate-600" htmlFor="">
-                Phone Number
-              </label>
-              <input
-                className="my-[8px] rounded-[8px] outline-none"
-                type="tel"
-                autoComplete="false"
-              />
+            <div className="flex pb-12 items-center justify-center gap-32">
+              <div className="flex flex-col">
+                <label className="text-sm font-bold text-slate-600" htmlFor="">
+                  Password
+                </label>
+                <input
+                  className="my-[8px] rounded-[8px] outline-none"
+                  type="password"
+                  placeholder="••••••••••"
+                  autoComplete="false"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label
+                  className="text-sm font-bold text-slate-600 gap-32"
+                  htmlFor=""
+                >
+                  Confirm Password
+                </label>
+                <input
+                  className="my-[8px] rounded-[8px] outline-none"
+                  type="password"
+                  autoComplete="false"
+                  placeholder="••••••••••"
+                />
+              </div>
             </div>
-          </div>
-          <div className="flex pb-12 items-center justify-center gap-32">
-            <div className="flex flex-col">
-              <label className="text-sm font-bold text-slate-600" htmlFor="">
-                Password
-              </label>
-              <input
-                className="my-[8px] rounded-[8px] outline-none"
-                type="password"
-                placeholder="••••••••••"
-                autoComplete="false"
-              />
+            <div className="flex pb-12 items-center justify-center gap-32">
+              <div className="flex flex-col">
+                <label className="text-sm font-bold text-slate-600" htmlFor="">
+                  Email
+                </label>
+                <input
+                  className="my-[8px] rounded-[8px] outline-none"
+                  type="email"
+                  value={email}
+                  placeholder="example@example.eg"
+                  autoComplete="false"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="text-sm font-bold text-slate-600" htmlFor="">
+                  Confirm Email
+                </label>
+                <input
+                  className="my-[8px] rounded-[8px] outline-none"
+                  type="email"
+                  placeholder="example@example.eg"
+                  autoComplete="false"
+                />
+              </div>
             </div>
-            <div className="flex flex-col">
-              <label
-                className="text-sm font-bold text-slate-600 gap-32"
-                htmlFor=""
+            <div className="flex justify-center">
+              <motion.button
+                whileTap={{ scale: 0.95 }}
+                className=" bg-emerald-500 hover:bg-emerald-600 text-white text-[12px] border-[1px] px-4 border-transparent min-h-12 rounded-full font-[600] tracking-[0.5px] uppercase cursor-pointer transition-all ease-linear duration-100"
               >
-                Confirm Password
-              </label>
-              <input
-                className="my-[8px] rounded-[8px] outline-none"
-                type="password"
-                autoComplete="false"
-                placeholder="••••••••••"
-              />
+                Update Info
+              </motion.button>
             </div>
-          </div>
-          <div className="flex pb-12 items-center justify-center gap-32">
-            <div className="flex flex-col">
-              <label className="text-sm font-bold text-slate-600" htmlFor="">
-                Email
-              </label>
-              <input
-                className="my-[8px] rounded-[8px] outline-none"
-                type="email"
-                autoComplete="false"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label className="text-sm font-bold text-slate-600" htmlFor="">
-                Confirm Email
-              </label>
-              <input
-                className="my-[8px] rounded-[8px] outline-none"
-                type="email"
-                autoComplete="false"
-              />
-            </div>
-          </div>
-          <div className="flex justify-center">
-            <motion.button
-              whileTap={{ scale: 0.95 }}
-              className=" bg-emerald-500 hover:bg-emerald-600 text-white text-[12px] border-[1px] px-4 border-transparent min-h-12 rounded-full font-[600] tracking-[0.5px] uppercase cursor-pointer transition-all ease-linear duration-100"
-            >
-              Update Info
-            </motion.button>
-          </div>
+          </form>
         </div>
       </section>
     </div>
