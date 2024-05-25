324d71fd7085fecf7c0c154fbcb68c6d925720c3
Mouaad <93816869+sMouaad@users.noreply.github.com>
2024-05-25 16:23:08 +0100

    Add messages initial frontend

--- App/Web/Home/src/Messages.jsx
+++ App/Web/Home/src/Messages.jsx
@@ -10,6 +10,7 @@ export default function Messages() {
   const [email, setEmail] = useState("user@email.com");
   const [role, setRole] = useState("user");
   const [picture, setPicture] = useState("");
+  const [patients, setPatients] = useState([]);
 
   useEffect(() => {
     Axios.get("http://localhost:3000/auth/userdata").then((res) => {
@@ -18,6 +19,9 @@ export default function Messages() {
         setEmail(res.data.email);
         setRole(res.data.type);
         setPicture(res.data.picture);
+        setPatients(
+          res.data.patientsCreated.concat(res.data.secondaryPatients)
+        );
       }
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -27,7 +31,28 @@ export default function Messages() {
     <div className="flex-wrap h-screen flex font-Roboto">
       <Sidebar setAuth={setAuth} role={role} />
       <section className="bg-slate-100 flex-[6] flex  flex-col">
-        <div>test</div>
+        <div className="flex flex-1 p-4 gap-4">
+          <div className="rounded-md bg-white shadow-xl flex-[2]"></div>
+          <div className="rounded-md bg-white shadow-xl flex-1 ">
+            <table className="w-full border-collapse">
+              <tr>
+                <th className="text-start py-4 border-none text-2xl font-bold">
+                  Patinets
+                </th>
+              </tr>
+
+              {patients.map((element) => {
+                return (
+                  <tr key={element._id}>
+                    <td className="text-lg transition-all ease-linear border-none hover:bg-slate-100 rounded-md hover:cursor-pointer h-16">
+                      {element.name}
+                    </td>
+                  </tr>
+                );
+              })}
+            </table>
+          </div>
+        </div>
       </section>
     </div>
   );
