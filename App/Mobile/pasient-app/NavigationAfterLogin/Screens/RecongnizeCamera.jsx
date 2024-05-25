5261fbc8fbbc55e5d4e4287ef3d1b2d531678d33
Mahdjoubabderraouf <mahdjoub.abderraouf@gmail.com>
2024-05-26 00:06:08 +0100

    integration of face recognation with front in patient app

--- App/Mobile/pasient-app/NavigationAfterLogin/Screens/RecongnizeCamera.jsx
+++ App/Mobile/pasient-app/NavigationAfterLogin/Screens/RecongnizeCamera.jsx
@@ -1,25 +1,38 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Text, View, StyleSheet, TouchableOpacity, Image } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Modal,
+  Image,
+  Pressable,
+  ActivityIndicator,
+} from "react-native";
 import Constants from "expo-constants";
-import { Camera } from "expo-camera";
+import { CameraView, Camera } from "expo-camera";
 import * as MediaLibrary from "expo-media-library";
-import { MaterialIcons } from "@expo/vector-icons";
+import Axios from "axios";
 import Button from "./ButtonCamera";
+import { getData } from "../../localStorage";
+
+// import useFetch from "../../MyHooks/useFetch";
 
 export function RecongnizeCamera() {
-  
   const [hasCameraPermission, setHasCameraPermission] = useState(null);
-
   const [image, setImage] = useState(null);
-
-  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
-
+  const [modalVisible, setModalVisible] = useState(false);
+  const [flashMode, setFlashMode] = useState(Camera.Constants?.FlashMode?.on);
   const cameraRef = useRef(null);
+  const [fetched, setFetsched] = useState(false);
+  const [fetchedData, setFetschedData] = useState({});
+  // const [refreshing, setRefreshing] = useState(true);
 
   useEffect(() => {
     (async () => {
       MediaLibrary.requestPermissionsAsync();
+
       const cameraStatus = await Camera.requestCameraPermissionsAsync();
+
       setHasCameraPermission(cameraStatus.status === "granted");
     })();
   }, []);
@@ -28,8 +41,41 @@ export function RecongnizeCamera() {
     if (cameraRef) {
       try {
         const data = await cameraRef.current.takePictureAsync();
+        setFetsched(false);
+        setModalVisible(true);
+
         console.log(data);
         setImage(data.uri);
+
+        const id = await getData("patientId");
+
+        const parts = data.uri.split("/");
+        const image_name = parts[parts.length - 1];
+
+        const formData = new FormData();
+        formData.append("file", {
+          uri: data.uri,
+          name: image_name,
+          type: "image/jpeg" || "image/png",
+        });
+
+        Axios.post(
+          `http://${process.env.FastAPI}/image_uploaded_by_patient/${id}`,
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        )
+          .then((response) => {
+            // console.log(response.data);
+            setFetschedData(response.data);
+            setFetsched(true);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
       } catch (error) {
         console.log(error);
       }
@@ -48,6 +94,14 @@ export function RecongnizeCamera() {
     }
   };
 
+  function toggleFlashMode() {
+    setFlashMode((current) =>
+      current === Camera.Constants?.FlashMode?.off
+        ? Camera.Constants?.FlashMode?.torch
+        : Camera.Constants?.FlashMode?.off
+    );
+  }
+
   if (hasCameraPermission === false) {
     return <Text>No access to camera</Text>;
   }
@@ -55,11 +109,11 @@ export function RecongnizeCamera() {
   return (
     <View style={styles.container}>
       {!image ? (
-        <Camera
+        <CameraView
           style={styles.camera}
-          type={Camera.Constants.Type.back}
+          type={Camera.Constants?.Type?.face}
           ref={cameraRef}
-          flashMode={flash}
+          facing={"back"}
         >
           <View
             style={{
@@ -69,20 +123,16 @@ export function RecongnizeCamera() {
             }}
           >
             <Button
-              onPress={() =>
-                setFlash(
-                  flash === Camera.Constants.FlashMode.off
-                    ? Camera.Constants.FlashMode.on
-                    : Camera.Constants.FlashMode.off
-                )
-              }
+              onPress={toggleFlashMode}
               icon="flash"
-              color={flash === Camera.Constants.FlashMode.off ? "gray" : "#fff"}
+              // color={
+              //   flash === Camera.Constants?.FlashMode?.off ? "gray" : "#fff"
+              // }
             />
           </View>
-        </Camera>
+        </CameraView>
       ) : (
-        <Image source={{ uri: image }} style={styles.camera} />
+        <Image source={{ uri: image }} style={styles.camera}></Image>
       )}
 
       <View style={styles.controls}>
@@ -105,6 +155,51 @@ export function RecongnizeCamera() {
           <Button title="Take a picture" onPress={takePicture} icon="camera" />
         )}
       </View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        // style={styles.centeredView}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        {!fetched && (
+          // <ScrollView
+          //   contentContainerStyle={styles.scrollViewActivityIndicator}
+          //   refreshControl={
+          //     <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          //   }
+          // >
+          <View className="flex-1 justify-center">
+            <ActivityIndicator size={"large"} />
+          </View>
+          // </ScrollView>
+        )}
+
+        {fetched && (
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.textStyle}>Hide Modale</Text>
+              </Pressable>
+              <View style={styles.messageContainer}>
+                <Image
+                  source={{ uri: fetchedData.url }}
+                  style={styles.profileImage}
+                />
+                <View style={styles.messageContent}>
+                  <Text style={styles.senderName}>{fetchedData.name}</Text>
+                  <Text style={styles.messageContent}>{fetchedData.who}</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        )}
+      </Modal>
     </View>
   );
 }
@@ -139,5 +234,60 @@ const styles = StyleSheet.create({
   },
   topControls: {
     flex: 1,
+  },
+  centeredView: {
+    justifyContent: "flex-end",
+    alignItems: "center",
+    flex: 1,
+  },
+  modalView: {
+    backgroundColor: "white",
+    margin: 20,
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+  messageContainer: {
+    flexDirection: "row",
+    padding: 20,
+    paddingHorizontal: 28,
+  },
+  profileImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 16,
+    marginRight: 10,
+    borderColor: "#654ff3",
+    borderWidth: 2,
+  },
+  messageContent: {
+    flex: 1,
+    marginHorizontal: 8,
+  },
+  senderName: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginHorizontal: 8,
+    marginVertical: 5,
   },
 });
--- App/Mobile/pasient-app/update_env.sh
+++ App/Mobile/pasient-app/update_env.sh
@@ -6,7 +6,8 @@ HOST_IP=$(hostname -I | awk '{print $1}')
 # Check if .env file exists
 if [ -f .env ]; then
     # Update SERVER_IP value in .env file
-    sed -i "s/SERVER_IP=.*/SERVER_IP=\"$HOST_IP:3000\"/" .env
+    sed -i "s/SERVER_IP=.*/SERVER_IP=\"$HOST_IP:3000\"/; s/FastAPI=.*/FastAPI=\"$HOST_IP:8000\"/" .env
+
     echo "SERVER_IP updated to $HOST_IP:3000"
 else
     echo ".env file not found!"
