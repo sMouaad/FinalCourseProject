2442fc8085ab7fafa6e4f91f2f29702acf822471
Mouaad <93816869+sMouaad@users.noreply.github.com>
2024-05-26 13:37:16 +0100

    Add new component support

---
+++ App/Web/Home/src/Support.jsx
@@ -0,0 +1,33 @@
+import useAuth from "./hooks/useAuth";
+import Axios from "axios";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import ProfilePic from "./assets/dashboard/pfp.svg";
+import { Sidebar } from "./Dashboard";
+
+export default function Support() {
+  const [name, setName] = useState("user");
+  const [email, setEmail] = useState("user@email.com");
+  const [role, setRole] = useState("user");
+  const [picture, setPicture] = useState("");
+  useEffect(() => {
+    Axios.get("http://localhost:3000/auth/userdata").then((res) => {
+      if (res.data.status) {
+        setName(res.data.name);
+        setEmail(res.data.email);
+        setRole(res.data.type);
+        setPicture(res.data.picture);
+      }
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  const { setAuth } = useAuth();
+  return (
+    <div className="flex-wrap h-screen flex font-Roboto">
+      <Sidebar setAuth={setAuth} role={role} />
+      <section className="bg-slate-100 flex-[6] flex  flex-col">
+        <div>hii</div>
+      </section>
+    </div>
+  );
+}
