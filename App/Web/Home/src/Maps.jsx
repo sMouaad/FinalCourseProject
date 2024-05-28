b35121090a44f028416e3df249348bb7c2de4c42
Mahdjoubabderraouf <mahdjoub.abderraouf@gmail.com>
2024-05-28 16:23:33 +0100

    Add: get location of the patient and send it to the server using socket.io

--- App/Web/Home/src/Maps.jsx
+++ App/Web/Home/src/Maps.jsx
@@ -1,33 +1,54 @@
 import { useState, useEffect } from "react";
-import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+import {
+  APIProvider,
+  InfoWindow,
+  Map,
+  Marker,
+} from "@vis.gl/react-google-maps";
 
 import io from "socket.io-client";
 import Brain from "./assets/pointer.svg";
+
 export default function Maps() {
-  // const [markerPos, setMarkerPos] = useState({});
+  const [markerPos, setMarkerPos] = useState({
+    lat: 36.7313881,
+    lng: 3.1832396,
+  });
+  const [open, setOpen] = useState(false);
+
   // const [socketIO, setSocketIO] = useState(null);
 
   useEffect(() => {
     const socket = io("http://localhost:3000");
 
-    // Send Message to the Server
-    socket.emit("msg", "Hello from the client!");
-
     // Event listener for receiving messages from the server
-    socket.on("gps", (msg) => {
-      console.log("message: " + msg);
+    socket.on("patientLoc", (msg) => {
+      setMarkerPos(msg);
     });
 
     return () => {
       socket.disconnect();
     };
   }, []);
-  const position = { lat: 36.68351349669975, lng: 3.0717719900568627 };
+  console.log(markerPos);
+  const position = { lat: markerPos.lat, lng: markerPos.lng };
   return (
-    <APIProvider apiKey="AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg">
+    <APIProvider apiKey={"AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg"}>
       <div className="h-screen">
-        <Map zoom={9} center={position}>
-          <Marker position={position} icon={Brain}></Marker>
+        <Map defaultZoom={20} defaultCenter={position}>
+          <Marker
+            position={position}
+            icon={Brain}
+            onClick={() => setOpen(true)}
+          />
+          {open && (
+            <InfoWindow
+              position={{ lat: markerPos.lat, lng: markerPos.lng }}
+              onCloseClick={() => setOpen(false)}
+            >
+              <div></div>
+            </InfoWindow>
+          )}
         </Map>
       </div>
     </APIProvider>
