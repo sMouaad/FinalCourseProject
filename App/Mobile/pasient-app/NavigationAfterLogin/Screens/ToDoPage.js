35a51c13e36c3b5131e5c9b5536b94cb10eb22be
Mahdjoubabderraouf <mahdjoub.abderraouf@gmail.com>
2024-06-03 10:52:34 +0100

    chore: Add missing console.log statement in toggleTaskCompletion function

--- App/Mobile/pasient-app/NavigationAfterLogin/Screens/ToDoPage.js
+++ App/Mobile/pasient-app/NavigationAfterLogin/Screens/ToDoPage.js
@@ -46,6 +46,7 @@ function ToDoPage({ navigation }) {
   }, []);
 
   const toggleTaskCompletion = (id) => {
+    console.log("")
     const newTasks = tasks.map((task) => {
       if (task._id === id) {
         Axios.post(`http://${SERVER_IP}/auth/check/`, {
