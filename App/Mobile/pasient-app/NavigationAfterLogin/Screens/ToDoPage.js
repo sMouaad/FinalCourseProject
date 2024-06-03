0edceb51968bc9a567f79578fe67d84799db2e2d
Mahdjoubabderraouf <mahdjoub.abderraouf@gmail.com>
2024-06-03 03:01:13 +0100

    add todo list for patient app

--- App/Mobile/pasient-app/NavigationAfterLogin/Screens/ToDoPage.js
+++ App/Mobile/pasient-app/NavigationAfterLogin/Screens/ToDoPage.js
@@ -1,33 +1,176 @@
-import React from 'react';
-import {View, Text, StyleSheet, Image, Pressable, Button, TouchableOpacity} from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+  StatusBar,
+} from "react-native";
 
+import Axios from "axios";
+import { SERVER_IP } from "@env";
+import { getData } from "../../localStorage";
 
-function ToDoPage()
-{
-    return (
-      <View  style={styles.container}>
-        <Text>ToDo Page!</Text>
-      </View>
+function ToDoPage({ navigation }) {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [patientId, setPatientId] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
 
-    );
+  const fetchData = async () => {
+    setLoading(true);
+    const patientId = await getData("patientId");
+    setPatientId(patientId);
+    Axios.get(`http://${SERVER_IP}/auth/get/${patientId}`)
+      .then((res) => {
+        if (res.data.status) {
+          setTasks(res.data.instructions);
+        }
+        setLoading(false);
+        setRefreshing(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+        setRefreshing(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const toggleTaskCompletion = (id) => {
+    const newTasks = tasks.map((task) => {
+      if (task._id === id) {
+        Axios.post(`http://${SERVER_IP}/auth/check/`, {
+          taskId: task._id,
+          patientId: patientId,
+        })
+          .then((res) => {
+            console.log(res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        return { ...task, done: !task.done };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  };
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    fetchData();
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.taskContainer}>
+      <TouchableOpacity onPress={() => toggleTaskCompletion(item._id)}>
+        <Text style={[styles.task, item.done && styles.completed]}>
+          {item.task}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <Text
+        className=" text-2xl text-center mx-4 font-medium rounded-[20px] text-[#4ff3af] bg-[#e1fffa] p-2 my-[17px] "
+        style={{
+          shadowColor: "#654ff3",
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          elevation: 15,
+        }}
+      >
+        Your To-do
+      </Text>
+      {loading ? (
+        <ActivityIndicator size="large" color="#00E5BD" />
+      ) : tasks.length ? (
+        <FlatList
+          style={styles.list}
+          data={tasks}
+          renderItem={renderItem}
+          keyExtractor={(item) => item._id.toString()}
+          onRefresh={onRefresh}
+          refreshing={refreshing}
+        />
+      ) : (
+        <Text>No tasks</Text>
+      )}
+    </View>
+  );
 }
+
 const styles = StyleSheet.create({
-  container:
-  {
+  container: {
+    marginTop: StatusBar.currentHeight,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    paddingHorizontal: 20,
   },
-  textSousImage: {
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  input: {
+    borderColor: "#00E5BD",
+    borderRadius: 21,
+    padding: 10,
+    flex: 1,
+    borderWidth: 3,
     fontSize: 15,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#9E9E9E',
-    marginBottom: 252,
   },
-}
-);
+  addButton: {
+    backgroundColor: "#00E5BD",
+    padding: 10,
+    borderRadius: 5,
+    marginLeft: 10,
+    borderRadius: 20,
+    borderWidth: 3,
+    borderColor: "black",
+  },
+  deleteButton: {
+    color: "red",
+    marginLeft: 10,
+    fontSize: 15,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  list: {
+    width: "100%",
+  },
+  taskContainer: {
+    flexDirection: "column",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  task: {
+    padding: 10,
+    fontSize: 15,
+    borderRadius: 21,
+    borderWidth: 3,
+    borderColor: "#00E5BD",
+  },
+  completed: {
+    textDecorationLine: "line-through",
+    color: "white",
+    backgroundColor: "#00E5BD",
+  },
+});
 
-
-export{ToDoPage};
+export { ToDoPage };
